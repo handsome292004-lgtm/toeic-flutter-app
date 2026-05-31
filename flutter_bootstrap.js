553ae6d07f22ff1d6dc340e7965511960870e7f1
handsome292004-lgtm <handsome292004-lgtm@users.noreply.github.com>
2026-05-31 04:38:58 +0000

    deploy: 2278f1b2879d15d5c32517a2e05aa16c3cc09c48

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3535397514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2353883750" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
